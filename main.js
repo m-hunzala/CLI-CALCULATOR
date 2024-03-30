57baa590bf9375b957d5904adba68f20ad308a91
m-hunzala <164901659+m-hunzala@users.noreply.github.com>
2024-03-30 06:47:36 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        type: "number",
+        name: "numberOne",
+        message: "enter your first number"
+    },
+    {
+        type: "number",
+        name: "numberTwo",
+        message: "enter your second number"
+    },
+    {
+        type: "list",
+        name: "operator",
+        choices: ["+", "-", "*", "/"],
+        message: "select operator",
+    },
+]);
+const { numberOne, numberTwo, operator } = answer;
+if (numberOne && numberTwo && operator) {
+    let result = 0;
+    if (operator === "+") {
+        result = numberOne + numberTwo;
+    }
+    else if (operator === "-") {
+        result = numberOne - numberTwo;
+    }
+    if (operator === "*") {
+        result = numberOne * numberTwo;
+    }
+    if (operator === "/") {
+        result = numberOne / numberTwo;
+    }
+    console.log("your result is:", result);
+}
+else {
+    console.log("place enter valid input");
+}
---
+++ main.ts
@@ -0,0 +1,45 @@
+import inquirer from "inquirer";
+
+const answer:{
+numberOne:number,
+numberTwo:number,
+operator:string
+}=await inquirer.prompt([
+{
+ type:"number",
+ name:"numberOne",
+ message:"enter your first number"
+
+},
+
+{
+    type:"number",
+    name:"numberTwo",
+    message:"enter your second number"
+       
+   },
+
+   {
+    type:"list",
+    name:"operator",
+    choices:["+","-","*","/"],
+    message:"select operator",
+       
+   },
+
+])
+   const{numberOne,numberTwo,operator} = answer
+   if(numberOne && numberTwo && operator){
+    let result:number=0;
+    if(operator === "+"){
+        result = numberOne+numberTwo
+    }else if(operator === "-"){
+        result = numberOne-numberTwo
+    }if(operator === "*"){
+        result = numberOne*numberTwo
+    }if(operator === "/"){
+        result = numberOne/numberTwo
+    }
+    console.log("your result is:",result)
+
+   }else{console.log("place enter valid input")}
